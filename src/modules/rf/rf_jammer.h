41a9933c832c51ca8e4cb76174cc111455185788
Ninja-jr <ninjajr1983@gmail.com>
2025-12-31 12:26:41 +0100

    Add new functions for optimized pulse and random pattern

--- src/modules/rf/rf_jammer.h
+++ src/modules/rf/rf_jammer.h
@@ -16,6 +16,8 @@ private:
     void display_banner();
     void run_full_jammer();
     void run_itmt_jammer();
+    void send_optimized_pulse(uint32_t width);
+    void send_random_pattern(uint32_t numPulses);
 };
 
 #endif

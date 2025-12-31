6ec4d93072c3252c960c3f9f077529fbb87363f3
Ninja-jr <ninjajr1983@gmail.com>
2025-12-31 12:40:23 +0100

    Update rf_jammer.h

--- src/modules/rf/rf_jammer.h
+++ src/modules/rf/rf_jammer.h
@@ -16,8 +16,8 @@ private:
     void display_banner();
     void run_full_jammer();
     void run_itmt_jammer();
-    void send_optimized_pulse(uint32_t width);
-    void send_random_pattern(uint32_t numPulses);
+    void send_optimized_pulse(int width);
+    void send_random_pattern(int numPulses);
 };
 
 #endif

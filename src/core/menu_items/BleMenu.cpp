f02b8f2356ed6ad572375515689d3dc58f823cb1
mokapi47 <vigouretmahe@gmail.com>
2026-02-17 14:06:26 +0100

    Add files via upload

--- src/core/menu_items/BleMenu.cpp
+++ src/core/menu_items/BleMenu.cpp
@@ -29,7 +29,9 @@ void BleMenu::optionsMenu() {
 #if !defined(LITE_VERSION)
     options.push_back({"Presenter", [=]() { PresenterMode(hid_ble, true); }});
     options.push_back({"BLE Scan", ble_scan});
-    options.push_back({"iBeacon", [=]() { ibeacon(); }});
+    options.push_back({"iBeacon", [=]() {
+                           ibeacon("Bruce", "e4c159a0-8c82-11e6-bdf4-0800200c9a66", 0x004C);
+                       }});
     options.push_back({"Bad BLE", [=]() { ducky_setup(hid_ble, true); }});
     options.push_back({"BLE Keyboard", [=]() { ducky_keyboard(hid_ble, true); }});
 #endif

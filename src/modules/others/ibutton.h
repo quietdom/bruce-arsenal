15ebce3f767c09dac1b0592b2c5a4ec7a46cfe14
Aleksandr Chumakov <delfer@bk.ru>
2026-03-16 19:10:30 +0300

    feat(iButton): enhance UI/UX and add file save/load support

--- src/modules/others/ibutton.h
+++ src/modules/others/ibutton.h
@@ -1,8 +1,16 @@
 #ifndef LITE_VERSION
+#pragma once
+
 #include <OneWire.h>
 
+enum IButtonResult {
+    IBUTTON_SUCCESS = 0,
+    IBUTTON_FAILURE,
+    IBUTTON_NO_KEY,
+    IBUTTON_CRC_ERROR,
+};
+
 void setup_ibutton();
-void write_ibutton();
-void read_ibutton();
 void setiButtonPinMenu();
+
 #endif
